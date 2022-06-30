11f2dd9a4e1a61509ea2e3cb06759a8f659e6ec4
stepkos <stepkos@wp.pl>
2022-06-30 22:46:52 +0200

    episode 2

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,31 @@
 from distutils.log import debug
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Jakub Stepkowski',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'June 30, 2022'
+    },
+    {
+        'author': 'Jan Kowalski',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 1, 2022'
+    },
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html', title='About')
 
 
 

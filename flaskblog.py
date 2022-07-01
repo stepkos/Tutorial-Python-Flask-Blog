7e57b3590ffcdb433379523571e861051b71e5bc
stepkos <stepkos@wp.pl>
2022-07-02 00:24:09 +0200

    episode 3

--- flaskblog.py
+++ flaskblog.py
@@ -20,18 +20,18 @@ posts = [
     },
 ]
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html', posts=posts)
 
 
-@app.route("/about")
+@app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -40,9 +40,15 @@ def register():
     return render_template('register.html', title='Register', form=form)
 
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Register', form=form)
 
 

f568b472962211b8b70905c3f585ab5054e9ccc7
stepkos <stepkos@wp.pl>
2022-07-02 17:59:27 +0200

    restructure files to package

---
+++ flaskblog/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '39022dcb1591401466fdfe2809f1fc2b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+from flaskblog import routes

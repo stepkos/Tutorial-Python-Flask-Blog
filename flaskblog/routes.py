1ce595d6ed2ca7b86777cf3f8ee6058f5e502f80
stepkos <stepkos@wp.pl>
2022-07-03 19:11:53 +0200

    [NEW] pagination

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -12,7 +12,8 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route('/')
 @app.route('/home')
 def home():
-    posts = Post.query.all()
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
 
 
@@ -140,3 +141,10 @@ def delete_post(post_id):
     db.session.commit()
     flash('Your post has been deleted!', 'success')
     return redirect(url_for('home'))
+
+
+# @app.route('/user/<>')
+# def home():
+#     page = request.args.get('page', 1, type=int)
+#     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+#     return render_template('home.html', posts=posts)

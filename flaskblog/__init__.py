6ab71687b9893be68eb4c4c19975a1abb37e2a63
stepkos <stepkos@wp.pl>
2022-07-03 02:10:16 +0200

    register and login page

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -8,6 +9,9 @@ app.config['SECRET_KEY'] = '39022dcb1591401466fdfe2809f1fc2b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 
 from flaskblog import routes

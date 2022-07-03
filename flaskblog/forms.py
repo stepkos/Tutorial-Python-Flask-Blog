2ef77c813cc98afddad8f7f473d26d52176e271d
stepkos <stepkos@wp.pl>
2022-07-03 15:13:30 +0200

    [NEW] adding posts

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms import validators as val
 from flaskblog.models import User
 
@@ -77,3 +77,9 @@ class UpdateAccountForm(FlaskForm):
     def validate_email(self, email):
         if email.data != current_user.email and User.query.filter_by(email=email.data).first():
             raise val.ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[val.DataRequired()])
+    content = TextAreaField('Content', validators=[val.DataRequired()])
+    submit = SubmitField('Post')

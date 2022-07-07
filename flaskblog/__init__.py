8bce9450761e0e93f24575ebc74a988f820f22a8
stepkos <stepkos@wp.pl>
2022-07-07 20:29:21 +0200

    Part 11 - Blueprints

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,16 +2,30 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flaskblog.config import Config
 
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '39022dcb1591401466fdfe2809f1fc2b'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
 
-from flaskblog import routes
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from flaskblog.users.routes import users
+    from flaskblog.posts.routes import posts
+    from flaskblog.main.routes import main
+    
+    app.register_blueprint(users)
+    app.register_blueprint(posts)
+    app.register_blueprint(main)
+
+    return app
---
+++ flaskblog/config.py
@@ -0,0 +1,6 @@
+import os
+
+
+class Config:
+    SECRET_KEY = '39022dcb1591401466fdfe2809f1fc2b'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
